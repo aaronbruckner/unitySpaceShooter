3a693e65b4b92bbe5d9686e8efb84b01409ef9e0
Aaron Bruckner <aaronbruckner@gmail.com>
2018-03-20 15:28:46 -0700

    Added enemy ship that can fire shots.

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public float startDelay;
+
+    private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        InvokeRepeating("Fire", startDelay, fireRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Fire() {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        audioSource.Play();
+    }
+}
