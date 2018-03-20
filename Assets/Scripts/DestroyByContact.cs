3d2231af390595b7b7a82eb8c8ea285186fee5bb
Aaron Bruckner <aaronbruckner@gmail.com>
2018-03-19 21:53:32 -0700

    Added score system

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,6 +6,18 @@ public class DestroyByContact : MonoBehaviour {
 
     public GameObject explosion;
     public GameObject playerExplosion;
+    public int scoreValue;
+
+    private GameController gameController;
+
+    void Start() {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null) {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        } else {
+            Debug.Log("Cannot find GameController instance");
+        }
+    }
 
 	void OnTriggerEnter(Collider other) {
         if (other.tag == "Boundary") {
@@ -16,6 +28,7 @@ public class DestroyByContact : MonoBehaviour {
         }
         Instantiate(explosion, transform.position, transform.rotation);
         Destroy(other.gameObject);
-        Destroy(gameObject);        
+        Destroy(gameObject);
+        gameController.AddScore(scoreValue);
     }
 }
